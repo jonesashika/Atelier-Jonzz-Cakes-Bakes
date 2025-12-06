d403bfebc534c5624bd3177eedb68480460f8893
Jones Ashika <jonesashika004@gmail.com>
2025-12-06 18:39:49 +0530

    add app.get

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,9 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get(/^(.*)$/, (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
+app.get('/', (req, res) => {
+  res.send('Backend is alive');
+});
 
 connectDB();
 app.use('/api', authRoutes);
